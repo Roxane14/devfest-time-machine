61720d899977c0af73b81f834cdea62f8f56bbce
Roxane Le Tensorer <roxane.le-tensorer@capgemini.com>
2023-09-06 18:51:18 +0200

    Deploy project to GitHub Pages

--- devfest-time-machine/src/App.tsx
+++ devfest-time-machine/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Eula from "./pages/Eula";
 import { ROUTES } from "./config/routes.constants";
@@ -7,13 +7,13 @@ import Category from "./pages/Category";
 
 function App() {
   return (
-  <Router>
+  <HashRouter>
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />}/>
       <Route path={ROUTES.EULA} element={<Eula />}/>
       <Route path={ROUTES.CATEGORY} element={<Category />}/>
     </Routes>
-  </Router>
+  </HashRouter>
   );
 }
 
