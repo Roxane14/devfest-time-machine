1e8dfc061a689a608485a365c12ac7fafd19bda8
Roxane Le Tensorer <roxane.le-tensorer@capgemini.com>
2023-09-06 19:11:15 +0200

    Deploy project to GitHub Pages

--- devfest-time-machine/src/config/routes.constants.ts
+++ devfest-time-machine/src/config/routes.constants.ts
@@ -1,11 +1,11 @@
 const ROUTES = {
-    HOME: "/",
-    EULA: "/eula",
-    CATEGORY: "/category",
-    QUESTION: "/question",
-    SUCCESS: "/success",
-    FAILURE: "/failure",
-    END: "/end"
+    HOME: "/devfest-time-machine",
+    EULA: "/devfest-time-machine/eula",
+    CATEGORY: "/devfest-time-machine/category",
+    QUESTION: "/devfest-time-machine/question",
+    SUCCESS: "/devfest-time-machine/success",
+    FAILURE: "/devfest-time-machine/failure",
+    END: "/devfest-time-machine/end"
 };
 
 export {
